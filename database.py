9325a2d299a6d6abebaadf0b0d8da6fc7b68f43e
Jake Scudder <scudsterr@gmail.com>
2021-06-28 16:52:15 -0400

    Basic CRUD operations

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ import mysql.connector
 
 config = {
   "user": "root",
-  
+  "password": "",
+  "host": "localhost",
+  "database": "python_sql_refresh"
 }
 
-db = mysql.connector.connect()
+db = mysql.connector.connect(**config)
+cursor = db.cursor()
